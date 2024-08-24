cdfc337b605210dd0042219e4dd3f47413958b4f
Anish <anishkumarak8686@gmail.com>
2024-08-24 12:41:25 +0530

    updated APP/frontend

--- APP/FRONTEND/lib/src/pages/Charging/Settings/stop_charger.dart
+++ APP/FRONTEND/lib/src/pages/Charging/Settings/stop_charger.dart
@@ -63,7 +63,7 @@ class _StopChargerState extends State<StopCharger> {
       }
     } catch (error) {
       print('Error fetching user details: $error');
-      _showAlertBanner('Error fetching user details');
+      _showAlertBanner('Internal server error ');
     }
   }
 
