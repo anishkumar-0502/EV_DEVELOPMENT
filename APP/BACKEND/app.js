407a56f3a02d96715575b5d2336039d96571c05e
Anish <anishkumarak8686@gmail.com>
2024-09-23 12:16:16 +0530

    updated

--- APP/BACKEND/app.js
+++ APP/BACKEND/app.js
@@ -14,22 +14,23 @@ const ChargingSession = require('./src/ChargingSession/routes.js');
 const wallet = require('./src/Wallet/routes.js');
 const sessionhistory = require('./src/SessionHistory/routes.js');
 const profile = require('./src/Profile/routes.js');
+const OcppConfig = require('./src/OcppConfig/routes.js');
+const SessionLog = require('./src/SessionLog/route.js');
 
 app.use(express.json());
-
 
 app.use((req, res, next) => {
     console.log(`${req.method} request for '${req.url}'`);
     next();
 });
 
-
 app.use('/', home);
 app.use('/charging', ChargingSession);
 app.use('/wallet', wallet);
 app.use('/session', sessionhistory);
 app.use('/profile', profile);
-
+app.use('/OcppConfig', OcppConfig);
+app.use('/sessionlog', SessionLog);
 
 // Create an HTTP server using Express app
 const httpServer = http.createServer(app);
